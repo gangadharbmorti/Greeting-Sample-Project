5a6a48fa24f60a9d2266ef3661408cf2a8169fbc
Gangadhar <gangadharbmorti@gmail.com>
2021-09-07 22:41:05 +0530

    sample greeting project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+  let currentDate = new Date();
+  currentDate = currentDate.getHours();
+  let greeting = '';
+  const cssStyle = {};
+
+  if (currentDate >= 1 && currentDate < 12) {
+    greeting = 'Good Morning';
+    cssStyle.color = 'orange';
+  } else if (currentDate >= 12 && currentDate < 17) {
+    greeting = 'Good Afternoon';
+    cssStyle.color = 'blue';
+  } else if (currentDate >= 17 && currentDate < 19) {
+    greeting = 'Good Evening';
+    cssStyle.color = 'red';
+  } else {
+    greeting = 'Good Night';
+    cssStyle.color = 'black';
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Hello Sir, <span style={cssStyle}> {greeting}</span>
+      </h1>
     </div>
   );
 }
